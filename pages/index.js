1ecaab5681f947352345ec7e495e09cc12be03fa
Luan Lima <56600173+Lmlima05@users.noreply.github.com>
2024-03-13 13:08:38 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Jenny, eu amo você. Se você me ama, dá uma risadinha!git </h1>
+    return <h1>Jenny, eu amo você. Se você me ama, dá uma risadinha!git :)</h1>
 }
 
 export default Home;
