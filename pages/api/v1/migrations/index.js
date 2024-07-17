67070109efc3aff65c11fc9ca21cdcf3adc10be2
Luan Melo <luanmelolima@gmail.com>
2024-07-17 20:30:56 -0300

    reading migrations that are pending

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,3 +1,14 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path"
+
 export default async function migrations(request, response) {
-  response.status(200).json([]);
+  const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra/migrations"),
+    direction: "up",
+    verbose: true,
+    migrationTable: "pgmigrations",
+  })
+  response.status(200).json(migrations);
 }
